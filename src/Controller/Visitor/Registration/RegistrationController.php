f815d779251a3c6f0bdf713efe4cf05fa07b3ad4
Alan <user@macbookpro.home>
2025-08-09 15:41:26 +0200

    ajout de la page connexion, du design de la page connexion et limitation d'erreur et remember me

--- src/Controller/Visitor/Registration/RegistrationController.php
+++ src/Controller/Visitor/Registration/RegistrationController.php
@@ -26,6 +26,10 @@ class RegistrationController extends AbstractController
     #[Route('/inscription', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_visitor_welcome');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationForm::class, $user);
         $form->handleRequest($request);
@@ -97,6 +101,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_visitor_welcome');
+        return $this->redirectToRoute('app_login');
     }
 }
